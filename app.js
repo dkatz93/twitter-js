ffa1a1972789d9a9d2f84d3523b04b837a7458b7
Danielle Katz <daniellekatz93@gmail.com>
2016-11-08 10:58:42 -0600

    changes through configuring express

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const app = express();
 
+
+
+app.use(function(req, res, next){
+	console.log('This will show up everywhere');
+	console.log(req.method, " / ", res.statusCode)
+	next();	
+})
+
+app.use('/special/', function(req, res, next){
+	console.log('This is special');
+	next();	
+})
+
 app.get('/', function(req, res){
 	res.send('Welcome to our website!')
 })
 
+app.get('/news', function(req, res){
+	res.send('News!')
+})
 
 app.listen(3000);
