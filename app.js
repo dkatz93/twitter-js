b3370c0132ac8a63b85ef9d054fdfa782bea7861
larry@vincek.com <lvincek@Larrys-MacBook-Pro.local>
2016-11-08 12:08:21 -0600

    lets eat

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const nunjucks = require('nunjucks');
+const tweets = require('./tweetBank');
+
 const app = express();
 
-// no one hears my hellos
-
-// hi back
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views', {noCache: true}); // point nunjucks to the proper directory for templates
 
 app.use(function(req, res, next){
 	console.log('This will show up everywhere');
@@ -17,7 +20,18 @@ app.use('/special/', function(req, res, next){
 })
 
 app.get('/', function(req, res){
-	res.send('Welcome to our website!')
+	var locals = {
+	    title: 'An Example',
+	    people: [
+	        { name: 'Gandalf'},
+	        { name: 'Frodo' },
+	        { name: 'Hermione'},
+	       	{ name: 'Kelly' },
+	        { name: 'James'}
+    	]
+	};
+	res.render('index', locals)
+	//res.send('Welcome to our website!')
 })
 
 app.get('/news', function(req, res){
